243c6dab9c156138746829393ba5d136423332e5
MineGumus <94003285+MineGumus@users.noreply.github.com>
2022-06-09 09:33:00 -0400

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-print("Hello World from Gumus Family!")
-=======
-print("Hello World from Michigan!")
->>>>>>> 2fc475b5ca5ab0c83ed2cb7d53a484a9cd904042
+print("Hello World from Gumus Family!)
+
